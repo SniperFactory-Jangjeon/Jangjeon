9151cdc3619a8fa024414f65053f06d9a7a16baa
jieun27 <zzmfha27@naver.com>
2023-04-28 16:02:29 +0900

    카카오 로그인 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,30 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:jangjeon/controller/login_controller.dart';
 import 'package:jangjeon/firebase_options.dart';
+import 'package:jangjeon/view/page/login_page.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-   await Firebase.initializeApp(
-   options: DefaultFirebaseOptions.currentPlatform,
- );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  KakaoSdk.init(nativeAppKey: 'cbe2327f360f8a91c80f74544db1800f');
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetMaterialApp(
+      initialBinding: BindingsBuilder(() {
+        Get.put(LoginController());
+      }),
+      home: LoginPage(),
+    );
   }
 }
