cc6116a0ebb4de1e3cb17d2801b1f4eb138763d8
Kim-kwan-woo <kw2577@naver.com>
2023-05-16 12:12:39 +0900

    sms 인증 구현 및 테스트 완료

--- lib/service/sms_service.dart
+++ lib/service/sms_service.dart
@@ -6,14 +6,13 @@ class SMSService {
 
   Future<String> requestVerificationCode(String receiver, String code) async {
     final response = await http.post(Uri.parse(url), body: <String, String>{
-      'key': 'api 키',
+      'key': 'api키 입력',
       'user_id': 'ymsco',
       'sender': '01056819450',
       'receiver': receiver,
       'msg': '장전 앱에서 전송한 인증번호입니다.\n$code를 사용해 인증을 진행하세요.',
       'testmode_yn ': 'Y',
     });
-    print(response.body);
 
     return response.body;
   }
