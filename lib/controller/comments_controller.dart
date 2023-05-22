662b690ab3204eeaea9565f0a13de4501b3b53de
Kim-kwan-woo <kw2577@naver.com>
2023-05-22 12:14:12 +0900

    댓글 더보기 ui 완성

--- lib/controller/comments_controller.dart
+++ lib/controller/comments_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart' as firebaseAuth;
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:jangjeon/controller/auth_controller.dart';
@@ -8,8 +9,11 @@ import 'package:jangjeon/service/db_service.dart';
 class CommentsController extends GetxController {
   final ticker = Get.arguments['ticker'];
   TextEditingController commentController = TextEditingController();
+  firebaseAuth.User user = Get.find<AuthController>().user!;
 
   RxList<Comment> comments = <Comment>[].obs;
+
+  RxBool isButtonActivated = false.obs;
 
   //댓글 등록하기
   createComment() async {
@@ -32,6 +36,15 @@ class CommentsController extends GetxController {
     var result = await DBService().readComments(ticker);
     comments.addAll(result);
     print(comments);
+  }
+
+  //댓글 작성하기 버튼 활성화
+  activeButton() {
+    if (commentController.text.isNotEmpty) {
+      isButtonActivated(true);
+    } else {
+      isButtonActivated(false);
+    }
   }
 
   @override
