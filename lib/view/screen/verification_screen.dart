628c64ce43a87d191d6271d524a5d4bb68f3e7d4
Kim-kwan-woo <kw2577@naver.com>
2023-05-30 09:51:44 +0900

    회원가입 성공 화면 패딩 조정

--- lib/view/screen/verification_screen.dart
+++ lib/view/screen/verification_screen.dart
@@ -11,139 +11,144 @@ class VerificationScreen extends GetView<SignupController> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  style: AppTextStyle.h2B28(),
-                  '본인인증',
-                ),
-                const SizedBox(height: 27),
-                Text(
-                  style: AppTextStyle.b3M16(),
-                  '이름',
-                ),
-                const SizedBox(height: 10),
-                AppTextField(
-                  controller: controller.nameController,
-                  hintText: '이름입력',
-                  onChanged: (_) => controller.activateNextButton(),
-                ),
-                const SizedBox(height: 18),
-                Text(
-                  style: AppTextStyle.b3M16(),
-                  '주민번호',
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  children: [
-                    Expanded(
-                      child: AppTextField(
-                        controller: controller.frontPriNumController,
-                        hintText: '생년월일',
-                        onChanged: (_) => controller.activateNextButton(),
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      child: AppTextField(
-                        obscureText: true,
-                        controller: controller.backPriNumController,
-                        hintText: '뒷자리',
-                        onChanged: (_) => controller.activateNextButton(),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 18),
-                Text(
-                  style: AppTextStyle.b3M16(),
-                  '통신사',
-                ),
-                const SizedBox(height: 10),
-                const AppToggleButton(),
-                const SizedBox(height: 18),
-                Text(
-                  style: AppTextStyle.b3M16(),
-                  '휴대폰번호',
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: AppTextField(
-                        controller: controller.phoneController,
-                        hintText: '-를 제외한 휴대폰번호 입력',
-                        onChanged: (_) => controller.checkPhoneValidation(),
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      flex: 1,
-                      child: Obx(
-                        () => AppElevatedButton(
-                          childText: '인증요청',
-                          onPressed: controller.isVerificationBtnActivated.value
-                              ? controller.requestVerificationCode
-                              : null,
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    style: AppTextStyle.h2B28(),
+                    '본인인증',
+                  ),
+                  const SizedBox(height: 27),
+                  Text(
+                    style: AppTextStyle.b3M16(),
+                    '이름',
+                  ),
+                  const SizedBox(height: 10),
+                  AppTextField(
+                    controller: controller.nameController,
+                    hintText: '이름입력',
+                    onChanged: (_) => controller.activateNextButton(),
+                  ),
+                  const SizedBox(height: 18),
+                  Text(
+                    style: AppTextStyle.b3M16(),
+                    '주민번호',
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: AppTextField(
+                          controller: controller.frontPriNumController,
+                          hintText: '생년월일',
+                          onChanged: (_) => controller.activateNextButton(),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 18),
-                Text(
-                  style: AppTextStyle.b3M16(),
-                  '인증번호',
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Obx(
-                        () => AppTextField(
-                          controller: controller.verificationCodeController,
-                          errorText: controller.verificationCodeError.value,
-                          hintText: '인증번호 입력',
-                          onChanged: (_) => controller.checkCodeValidation(),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: AppTextField(
+                          obscureText: true,
+                          controller: controller.backPriNumController,
+                          hintText: '뒷자리',
+                          onChanged: (_) => controller.activateNextButton(),
                         ),
                       ),
-                    ),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      flex: 1,
-                      child: Obx(
-                        () => AppElevatedButton(
-                          childText: '확인',
-                          onPressed: controller.isConfirmCodeBtnActivated.value
-                              ? controller.checkVerificationCode
-                              : null,
+                    ],
+                  ),
+                  const SizedBox(height: 18),
+                  Text(
+                    style: AppTextStyle.b3M16(),
+                    '통신사',
+                  ),
+                  const SizedBox(height: 10),
+                  const AppToggleButton(),
+                  const SizedBox(height: 18),
+                  Text(
+                    style: AppTextStyle.b3M16(),
+                    '휴대폰번호',
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: AppTextField(
+                          controller: controller.phoneController,
+                          hintText: '-를 제외한 휴대폰번호 입력',
+                          onChanged: (_) => controller.checkPhoneValidation(),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                      const SizedBox(width: 10),
+                      Expanded(
+                        flex: 1,
+                        child: Obx(
+                          () => AppElevatedButton(
+                            childText: '인증요청',
+                            onPressed:
+                                controller.isVerificationBtnActivated.value
+                                    ? controller.requestVerificationCode
+                                    : null,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 18),
+                  Text(
+                    style: AppTextStyle.b3M16(),
+                    '인증번호',
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Obx(
+                          () => AppTextField(
+                            controller: controller.verificationCodeController,
+                            errorText: controller.verificationCodeError.value,
+                            hintText: '인증번호 입력',
+                            onChanged: (_) => controller.checkCodeValidation(),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        flex: 1,
+                        child: Obx(
+                          () => AppElevatedButton(
+                            childText: '확인',
+                            onPressed:
+                                controller.isConfirmCodeBtnActivated.value
+                                    ? controller.checkVerificationCode
+                                    : null,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        const SizedBox(height: 20),
-        Obx(
-          () => AppElevatedButton(
-            childText: '다음단계',
-            onPressed: controller.isNextBtnActivated.value
-                ? () => controller.jumpToPage(1)
-                : null,
+          const SizedBox(height: 20),
+          Obx(
+            () => AppElevatedButton(
+              childText: '다음단계',
+              onPressed: controller.isNextBtnActivated.value
+                  ? () => controller.jumpToPage(1)
+                  : null,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
